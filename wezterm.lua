a2587106d692ac99e9b4b53bb9b4285c2e4f8a9a
Kushashwa Ravi Shrimali <kushashwaravishrimali@gmail.com>
2023-11-03 12:38:20 +0530

    minor changes in wezterm config - much needed

--- wezterm.lua
+++ wezterm.lua
@@ -24,7 +24,7 @@ table.insert(mykeys, { key = "s", mods = "ALT", action = act.Search({ CaseSensit
 
 table.insert(mykeys, { key = "f", mods = "ALT", action = "ToggleFullScreen" })
 
-table.insert(mykeys, { key = "n", mods = "ALT", action = act({ SpawnTab = "CurrentPaneDomain" }) })
+table.insert(mykeys, { key = "t", mods = "ALT", action = act({ SpawnTab = "CurrentPaneDomain" }) })
 
 table.insert(mykeys, { key = "[", mods = "ALT", action = act.MoveTabRelative(-1) })
 
@@ -38,12 +38,16 @@ table.insert(mykeys, { key = "j", mods = "ALT", action = act({ ActivatePaneDirec
 table.insert(mykeys, { key = "k", mods = "ALT", action = act({ ActivatePaneDirection = "Up" }) })
 table.insert(mykeys, { key = "l", mods = "ALT", action = act({ ActivatePaneDirection = "Right" }) })
 
-table.insert(mykeys, { key = "h", mods = "ALT|SHIFT", action = act({ AdjustPaneSize = { "Left", 1 } }) })
-table.insert(mykeys, { key = "j", mods = "ALT|SHIFT", action = act({ AdjustPaneSize = { "Down", 1 } }) })
-table.insert(mykeys, { key = "k", mods = "ALT|SHIFT", action = act({ AdjustPaneSize = { "Up", 1 } }) })
-table.insert(mykeys, { key = "l", mods = "ALT|SHIFT", action = act({ AdjustPaneSize = { "Right", 1 } }) })
+config.leader = { key = "a", mods = "CTRL" }
+table.insert(mykeys, { key = "h", mods = "SHIFT|ALT", action = act({ AdjustPaneSize = { "Left", 3 } }) })
+table.insert(mykeys, { key = "j", mods = "SHIFT|ALT", action = act({ AdjustPaneSize = { "Down", 3 } }) })
+table.insert(mykeys, { key = "k", mods = "SHIFT|ALT", action = act({ AdjustPaneSize = { "Up", 3 } }) })
+table.insert(mykeys, { key = "l", mods = "SHIFT|ALT", action = act({ AdjustPaneSize = { "Right", 3 } }) })
 
-config.font = wezterm.font("Iosevka", { weight = "Medium", italic = false, stretch = "Normal" })
+-- config.font = wezterm.font("Iosevka", { weight = "Medium", italic = false, stretch = "Normal" })
+config.line_height = 1.10
+-- config.cell_width = 1.0
+config.font = wezterm.font("DankMono Nerd Font", {weight="Regular", italic = false, stretch="Normal", style="Normal"}) -- /Library/Fonts/DankMonoNerdFont-Regular.ttf, CoreText
 -- config.font = wezterm.font("Iosevka", { weight = "Medium", italic = false, stretch = "Normal" })
 config.font_size = 20.0
 
@@ -105,7 +109,8 @@ config.hide_tab_bar_if_only_one_tab = true
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = false
 
-config.window_background_opacity = 0.89
-config.macos_window_background_blur = 30
+config.window_background_opacity = 1.0
+config.macos_window_background_blur = 50
+config.color_scheme = "Catppuccin Mocha"
 
 return config
